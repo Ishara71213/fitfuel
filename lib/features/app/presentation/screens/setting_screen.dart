37f26228412407ae24df3837e27cde0a160e5a50
Ishara2810 <57606275+Ishara71213@users.noreply.github.com>
2024-02-08 15:39:05 +0530

    push function to upload data to firebase

--- lib/features/app/presentation/screens/setting_screen.dart
+++ lib/features/app/presentation/screens/setting_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitfuel/core/components/app_bar_with_back_btn.dart';
+import 'package:fitfuel/core/constants/firebase_collections.dart';
 import 'package:fitfuel/core/enums/states.dart';
 import 'package:fitfuel/core/mock_json/schedule.dart';
 import 'package:fitfuel/core/widgets_library/widgets_library.dart';
@@ -18,10 +19,15 @@ class _SettingScreenState extends State<SettingScreen> {
   final firestore = FirebaseFirestore.instance;
   States uploadState = States.initial;
 
+  //Fire base data upload
   void uploadData() async {
     CollectionReference userCollectionRef;
-    userCollectionRef = firestore.collection("Users");
+    //Change collection Name to Collection that you need to push data into
+    userCollectionRef = firestore.collection(FirebaseCollections.schedules);
     late Map<String, dynamic> payload;
+    //Change Payload value into Map file located in Core/Mock Json folder
+    //Create Map<String,Dynamic> yourDataMapName
+    //insert data and Import it and replace the payload value
     payload = scheduleMock;
     setState(() {
       uploadState = States.loading;
